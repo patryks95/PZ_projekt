8ac1f1c5fd44d2e733fd948861c06b8b6a49cbef
s0152942 <s0152942@ANT020>
2017-04-13 13:09:51 +0200

    Add project files.

---
+++ PZ_projekt/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PZ_projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public string _File1Stream { get; set; }
+        public string _File2Stream { get; set; }
+
+        public string _File1Name { get; set; }
+        public string _File2Name { get; set; }
+        private int _MatrixCount { get; set; }
+
+        List<string> one;
+        List<string> two;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|Out files (*.out)|*.out";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    listFiles.Items.Add(System.IO.Path.GetFileName(filename));
+            }
+
+        }
+
+        private void Usuń_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+}
