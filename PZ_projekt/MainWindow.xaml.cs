7fd46261205bb4bf4bae9a5cb5c7cf17a0dac36e
Patryk <patryk-smol@wp.pl>
2017-04-13 23:37:17 +0200

    Poprawa wydajności cz. 1

--- PZ_projekt/MainWindow.xaml.cs
+++ PZ_projekt/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace PZ_projekt
 {
@@ -24,11 +25,12 @@ namespace PZ_projekt
     {
 
 
-        public string _File1Stream { get; set; }
-        public string _File2Stream { get; set; }
+        private OpenFileDialog openFileDialogFile1=new OpenFileDialog();
+        private string _File1Stream { get; set; }
+        private string _File2Stream { get; set; }
 
-        public string _File1Name { get; set; }
-        public string _File2Name { get; set; }
+        private string _File1Name { get; set; }
+        private string _File2Name { get; set; }
         private int _MatrixCount { get; set; }
 
         List<string> one;
@@ -41,21 +43,250 @@ namespace PZ_projekt
 
         private void Dodaj_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "Text files (*.txt)|*.txt|Out files (*.out)|*.out";
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            if (openFileDialog.ShowDialog() == true)
+            if (openFileDialogFile1.ShowDialog() == true)
             {
-                foreach (string filename in openFileDialog.FileNames)
-                    listFiles.Items.Add(System.IO.Path.GetFileName(filename));
+                if (openFileDialogFile1.FileName.IndexOf(".out") > -1)
+                {
+                    if (string.IsNullOrEmpty(_File1Stream))
+                    {
+                        _File1Stream = openFileDialogFile1.FileName;
+                        _File1Name = _File1Stream.Substring(_File1Stream.LastIndexOf('\\') + 1);
+                        labelFile1.Content += _File1Name;
+                        labelFile1.Visibility = Visibility.Visible;
+                        Usuń1.Visibility = Visibility.Visible;
+
+                    }
+                    else if (string.IsNullOrEmpty(_File1Stream) || string.IsNullOrEmpty(_File2Stream))
+                    {
+                        if (_File1Stream != openFileDialogFile1.FileName)
+                        {
+                            _File2Stream = openFileDialogFile1.FileName;
+                            _File2Name = _File2Stream.Substring(_File2Stream.LastIndexOf('\\') + 1);
+                            labelFile2.Content += _File2Name;
+                            labelFile2.Visibility = Visibility.Visible;
+                            Usuń2.Visibility=Visibility.Visible;
+                            Generuj.IsEnabled = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Podany plik już instnieje", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dodano maksymalna liczbe plików", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    }
+                }
+                else
+                {
+
+                    MessageBox.Show("Błędny format pliku", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+
+
+                }
             }
 
         }
 
-        private void Usuń_Click(object sender, RoutedEventArgs e)
+
+
+        private void Generuj_Click(object sender, RoutedEventArgs e)
         {
-          
+            System.IO.StreamReader sr = new
+            System.IO.StreamReader(this._File1Stream);
+            List<string> temp = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                temp.Add(sr.ReadLine());
+            }
+            one = dane(temp.ToArray<string>());
+
+
+            _MatrixCount = Convert.ToInt32(one[(one.Count - 4)]);
+            for (int i = 1; i <= _MatrixCount; i++)
+            {
+                
+                comboBoxpodmacierz.Items.Add(i);
+            }
+            sr.Close();
+
+            sr = new
+               System.IO.StreamReader(_File2Stream);
+            temp = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                temp.Add(sr.ReadLine());
+            }
+            two = dane(temp.ToArray<string>());
+
+            sr.Close();
+            comboBoxpodmacierz.SelectedItem = 1;
+
+            
+
+        }
+        Expander temp;
+        private void dodaj_do_panelu()
+        {
+            panel.Children.Clear();
+            for (int i = 0; i < one.Count; i += 4)
+            {
+                if (Convert.ToInt32(one[i]) == Convert.ToInt32(comboBoxpodmacierz.SelectedItem.ToString()))
+                {
+                    temp = new Expander();
+                    temp.Header = string.Format("{0,-10}{1,-15}{2,-15}", one[i], one[i + 2], one[i + 3]);
+                    StackPanel m_panel = new StackPanel();
+                    temp.Content = m_panel;
+
+                    temp.Expanded += (s, e) => 
+                    {
+                        temp.Content = m_panel;
+                        string tytul = ((Expander)s).Header.ToString();
+                        List<string> obliczone = oblicz(tytul.Substring(0, 10), tytul.Substring(10, 15), two);
+                        foreach(string obliczenia in obliczone)
+                        {
+                            TextBox dodany = new TextBox();
+                            dodany.Text = obliczenia;
+                            m_panel.Children.Add(dodany);
+                        }
+                    };
+                    
+                    panel.Children.Add(temp);
+                }
+            }
+
+        }
+        private void Usuń1_Click(object sender, RoutedEventArgs e)
+        {
+            labelFile1.Content = "Plik1:  ";
+            if (string.IsNullOrEmpty(this._File2Stream))
+            {
+                this._File1Name = null;
+                this._File1Stream = null;
+                labelFile1.Visibility = Visibility.Hidden;
+                Usuń1.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                labelFile2.Content = "Plik2:  ";
+                this._File1Name = this._File2Name;
+                labelFile1.Content += this._File1Name;
+                this._File1Stream = this._File2Stream;
+                this._File2Name = null;
+                this._File2Stream = null;
+                labelFile2.Visibility = Visibility.Hidden;
+                Usuń2.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Usuń2_Click(object sender, RoutedEventArgs e)
+        {
+            labelFile2.Content = "Plik2:";
+            this._File2Name = null;
+            this._File2Stream = null;
+            labelFile2.Visibility = Visibility.Hidden;
+            Usuń2.Visibility = Visibility.Hidden;
+        }
+
+
+        static List<string> dane(string[] tab)
+        {
+            string[] dane = null;
+            List<string> lista = new List<string>();
+            string pattern = @"^\s[1-9]\s+[1-9][0-9]*\s+[-]*[1-9]+[0-9]+.[0-9][0-9]*";
+            string pattern1 = @"^\s[1-9]\s+[1-9][0-9]*\s+[-]*[1-9]+[0-9]+.[0-9][0-9]*\s+[-]*[1-9][0-9]*.[0-9][0-9]*.{0,71}";
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            Regex rgx1 = new Regex(pattern1, RegexOptions.IgnoreCase);
+            for (int i = 0; i < tab.Length; i++)
+            {
+                string input;
+                input = tab[i];
+                MatchCollection matches = rgx.Matches(input);
+                MatchCollection matches1 = rgx1.Matches(input);
+                if (matches.Count > 0)
+                {
+                    int n = 0;
+                    //Console.WriteLine("{0} ({1} matches):", input, matches.Count);
+                    foreach (Match match in matches)
+                    {
+                        //Console.WriteLine("   " + match.Value);
+                        String[] splitTab = match.Value.Split(' ');
+                        for (int j = 0; j < splitTab.Length; j++)
+                        {
+                            if (splitTab[j] != "")
+                            {
+
+                                Console.WriteLine(splitTab[j]);
+                                lista.Add(splitTab[j]);
+
+                            }
+
+                        }
+                        foreach (Match match1 in matches1)
+                        {
+                            Console.WriteLine(match1.Value.Substring(match1.Value.Length - 10));
+                            lista.Add(match1.Value.Substring(match1.Value.Length - 10));
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        static List<String> oblicz(String numer_macierzy, string energia, List<string> Two)
+        {
+            List<String> wynik = new List<String>();
+            for (int i = 0; i < Two.Count; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    if (Int32.Parse(numer_macierzy) - 1 == Int32.Parse(Two[i]) || Int32.Parse(numer_macierzy) == Int32.Parse(Two[i]) || Int32.Parse(numer_macierzy) + 1 == Int32.Parse(Two[i]))
+                    {
+
+                        Double Energia_one = Double.Parse(energia.Replace(".", ","));
+                        Double Energia_two = Double.Parse(Two[i + 2].Replace(".", ","));
+                        Double Liczba_falowa = Math.Abs(Energia_two - Energia_one);
+                        Double dlugosc=0;
+                        if (Liczba_falowa >= 50000)
+                        {
+                            dlugosc = 1000000000 / Liczba_falowa;
+                        }
+                        if (Liczba_falowa < 50000)
+                        {
+                            Double n = 1 + (8060.51 + 2480990 / (132.2474 - Math.Pow((Liczba_falowa / 10000), 2) + 17455.7 / (39.32957 - Math.Pow((Liczba_falowa / 1000), 2)))) * 0.00000001;
+                            dlugosc = 1000000000 / (Liczba_falowa * n);
+
+
+                        }
+                        if ((Energia_two - Energia_one) > 0) {
+                            wynik.Add(String.Format("{0,-10}{1,-15}{2,-15}{3,-15}{4,-10}{5,-10}", Two[i].ToString(), Two[i + 2].ToString(), Two[i + 3].ToString(), Liczba_falowa.ToString(), dlugosc.ToString(), "↑"));
+                        }
+                        if ((Energia_two - Energia_one) == 0)
+                        {
+                            wynik.Add(String.Format("{0,-10}{1,-15}{2,-15}{3,-15}{4,-10}{5,-10}", Two[i].ToString(), Two[i + 2].ToString(), Two[i + 3].ToString(), Liczba_falowa.ToString(), dlugosc.ToString(), "◊"));
+                        }
+                        if ((Energia_two - Energia_one) < 0)
+                        {
+                            wynik.Add(String.Format("{0,-10}{1,-15}{2,-15}{3,-15}{4,-10}{5,-10}", Two[i].ToString(), Two[i + 2].ToString(), Two[i + 3].ToString(), Liczba_falowa.ToString(), dlugosc.ToString(), "↓"));
+                        }
+
+
+
+                    }
+
+
+                }
+
+
+
+            }
+
+            return wynik;
+        }
+
+        private void comboBoxpodmacierz_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dodaj_do_panelu();
         }
     }
 }
